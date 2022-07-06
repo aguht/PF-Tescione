a5de33cd0d1f9ebc685f6037892b3e0ce7567e6d
aguht <104039614+aguht@users.noreply.github.com>
2022-07-05 23:17:01 -0300

    update

---
+++ src/app/shared/services/alumnos.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlumnosService {
+
+  constructor() { 
+
+    //https://62c4cf7aabea8c085a7cd69c.mockapi.io/:endpoint
+  }
+}
